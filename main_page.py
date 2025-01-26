cf17718bc850685756a80f71085310e6467b0855
Francis <65022017+Fran-6@users.noreply.github.com>
2025-01-26 16:18:35 +0100

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -6,13 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-st.markdown("# Prénoms français :smile:")
+st.markdown("# Prénoms français donnés à la naissance :smile:")
 st.sidebar.markdown("# Présentation :balloon:")
 
 # Read the Parquet file into a DataFrame
 df = pd.read_parquet("prenoms-2023-dpt.parquet")
 
-st.write(df.head(5))
+st.write(df.head(6))
 
 
 
